7896736a7decab6115e1f5542debc52c9ea01b89
Andrea <andrea@interhact.net>
2023-12-14 15:15:02 -0600

    switch challenge

--- switches_challenge/Program.cs
+++ switches_challenge/Program.cs
@@ -52,3 +52,49 @@ Console.WriteLine($"Product: {size} {color} {type}");
 
 //updated code using switch statement
 
+switch (product[0])
+{
+    case "01":
+        type = "Sweat shirt";
+        break;
+    case "02":
+        type = "T-Shirt";
+        break;
+    case "03":
+        type = "Sweat shirt";
+        break;
+    default: 
+        type = "Other";
+        break;
+}
+
+switch (product[1])
+{
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "White";
+        break;
+}
+
+switch (product[2])
+{
+    case "S":
+       size = "Small";
+       break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size Fits All";
+        break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
