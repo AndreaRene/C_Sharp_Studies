906dbd24a48db3c82666bd150d4f6b0139542c64
Andrea <andrea@interhact.net>
2023-12-14 15:33:17 -0600

    complete for statement lesson and set up challenge

--- for_statements/Program.cs
+++ for_statements/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+for (int i = 10; i >= 0; i--)
+{
+    Console.WriteLine(i);
+}
+
+for (int i = 0; i < 10; i += 3)
+{
+    Console.WriteLine(i);
+}
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+    if (i == 7) break;
+}
+
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = names.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(names[i]);
+}
+
+// string[] names = { "Alex", "Eddie", "David", "Michael" };
+// foreach (var name in names)
+// {
+//     // Can't do this:
+//     if (name == "David") name = "Sammy";
+// }
+
+string[] names2 = { "Alex", "Eddie", "David", "Michael" };
+for (int i = 0; i < names2.Length; i++)
+    if (names2[i] == "David") names2[i] = "Sammy";
+
+foreach (var name in names2) Console.WriteLine(name);
