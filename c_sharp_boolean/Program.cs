fae13c583b900226e2ddcad819e598ec1bc7e895
AndreaRene <andrea@interhact.net>
2023-12-14 10:29:01 -0600

    work for my course

---
+++ c_sharp_boolean/Program.cs
@@ -0,0 +1,9 @@
+﻿string permission = "Employee";
+int level = 100;
+
+if (permission.Contains("Admin"))
+    Console.WriteLine((level >= 55) ? "Welcome, Super Admin User." : "Welcome, Admin user.");
+else if (permission.Contains("Manager"))
+    Console.WriteLine((level >= 20) ? "Contact an admin for access." : "You do not have sufficient privileges.");
+else
+    Console.WriteLine("You do not have sufficient privileges.");
