6d96fadd20654fcae65b7fe530ba4abdabce093f
Andrea <andrea@interhact.net>
2023-12-14 19:35:37 -0600

    game rules

--- rpg_game_battle_challenge/Program.cs
+++ rpg_game_battle_challenge/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//hero hp 10pts
+//monster hp 10pts
+//attacks valued 1-10
+//hero attacks first
+//monster attacks if health > 0
+//print amount of health lost and remaining
+//either to 0 or less print winner
