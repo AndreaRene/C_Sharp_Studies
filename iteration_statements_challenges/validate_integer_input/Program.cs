2e2f00d3eaf167170b95aab4abd0093d4c298762
Andrea <andrea@interhact.net>
2023-12-15 13:29:54 -0600

    validate interger challenge complete

--- iteration_statements_challenges/validate_integer_input/Program.cs
+++ iteration_statements_challenges/validate_integer_input/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+do {
+    Console.WriteLine("Please provide a value between 5 and 10:");
+    string input = Console.ReadLine() ?? "";
+
+    if (int.TryParse(input, out int numValue) && numValue >= 5 && numValue <= 10) {
+        Console.WriteLine($"Input {numValue} accepted.");
+        break;
+    } else {
+        Console.WriteLine($"Invalid input.");
+    }
+
+} while (true);
