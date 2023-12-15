aee3762b0404f048897146fb94143797874b4c57
Andrea <andrea@interhact.net>
2023-12-14 20:52:18 -0600

    game challenge complete

--- rpg_game_battle_challenge/Program.cs
+++ rpg_game_battle_challenge/Program.cs
@@ -5,3 +5,19 @@
 //monster attacks if health > 0
 //print amount of health lost and remaining
 //either to 0 or less print winner
+
+int heroHP = 10;
+int monsterHP = 10;
+Random random = new Random();
+
+do {
+    int swing = random.Next(1,10);
+    monsterHP -= swing;
+    Console.WriteLine($"Monster takes {swing} damage. Monster HP is {monsterHP}");
+    if(monsterHP <= 0) continue;
+    swing = random.Next(1,10);
+    heroHP -= swing;
+    Console.WriteLine($"Hero takes {swing} damage. Hero HP is {heroHP}");
+}while (monsterHP > 0  && heroHP > 0);
+
+Console.WriteLine(heroHP > monsterHP ? "Hero wins!" : "Monster wins!");
