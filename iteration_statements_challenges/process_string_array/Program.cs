4262bb935d4e920e98ff00efc6d1f9ac014a3725
Andrea <andrea@interhact.net>
2024-01-02 15:59:19 -0600

    work on indexof method in outer loop

--- iteration_statements_challenges/process_string_array/Program.cs
+++ iteration_statements_challenges/process_string_array/Program.cs
@@ -4,17 +4,18 @@ string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like
 
 // Your solution must declare an integer variable named periodLocation that can be used to hold the location of the period character within a string.
 
-int periodLocation = 0;
+int periodLocation;
 
 // Your solution must include an outer foreach or for loop that can be used to process each string element in the array. The string variable that you'll process inside the loops should be named myString.
 
 foreach (string myString in myStrings){
-
+    periodLocation = 0;
+    while (periodLocation != -1) {
+        periodLocation = myString.IndexOf(".", periodLocation + 1);
+        Console.WriteLine($"{periodLocation} in {myString}");
+        
+    }
 // In the outer loop, your solution must use the IndexOf() method of the String class to get the location of the first period character in the myString variable. The method call should be similar to: myString.IndexOf("."). If there's no period character in the string, a value of -1 will be returned.
-
-}
-
-
 
 // Your solution must include an inner do-while or while loop that can be used to process the myString variable.
 
@@ -23,3 +24,5 @@ foreach (string myString in myStrings){
 // In the inner loop, your solution must not display the period character.
 
 // In the inner loop, your solution must use the Remove(), Substring(), and TrimStart() methods to process the string information.
+
+}
