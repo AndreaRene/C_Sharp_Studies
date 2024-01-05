4906fa220d07d325e6462fb5856534f6c9e349b4
Andrea <andrea@interhact.net>
2024-01-05 10:41:17 -0600

    add loop to gather animal nickname

--- Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/Program.cs
+++ Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/Program.cs
@@ -228,6 +228,13 @@ do
                                 }
                             }
                         } while (animalNickname == "");
+                        // store the pet information in the ourAnimals array (zero based)
+                        ourAnimals[petCount, 0] = "ID #: " + animalID;
+                        ourAnimals[petCount, 1] = "Species: " + animalSpecies;
+                        ourAnimals[petCount, 2] = "Age: " + animalAge;
+                        ourAnimals[petCount, 3] = "Nickname: " + animalNickname;
+                        ourAnimals[petCount, 4] = "Physical description: " + animalPhysicalDescription;
+                        ourAnimals[petCount, 5] = "Personality: " + animalPersonalityDescription;
                     }
                 } while (validEntry == false);
                 petCount = petCount + 1;
