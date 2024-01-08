d73f0a38d8d2ce28a746536cc8d7fe1f506740ee
Andrea <andrea@interhact.net>
2024-01-08 15:16:47 -0600

    completed second part of challenge

--- casting_and_converting_math_challenge/Program.cs
+++ casting_and_converting_math_challenge/Program.cs
@@ -9,7 +9,9 @@ int result1 = value1/Convert.ToInt32(value2);
 Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
 
 // // Your code here to set result2
-// Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+decimal result2 = value2/Convert.ToDecimal(value3);
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
 // // Your code here to set result3
 // Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
