7f6e72f5d5e0cab4baffcab3b4f4aa17a2b3e24e
Andrea <andrea@interhact.net>
2024-01-10 14:09:11 -0600

    clearing stored values in array

--- arrays_and_helpers/Program.cs
+++ arrays_and_helpers/Program.cs
@@ -9,10 +9,23 @@ foreach (var pallet in pallets)
     Console.WriteLine($"-- {pallet}");
 }
 
+// sort the array in reverse
+
 Console.WriteLine("");
 Console.WriteLine("Reversed...");
 Array.Reverse(pallets);
 foreach (var pallet in pallets)
 {
     Console.WriteLine($"-- {pallet}");
+}
+
+// clear values stored 
+string[] palletsTwo = { "B14", "A11", "B12", "A13" };
+Console.WriteLine("");
+// starting with declared index and continuing for decelared amount of values
+Array.Clear(palletsTwo, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {palletsTwo.Length}");
+foreach (var palletTwo in palletsTwo)
+{
+    Console.WriteLine($"-- {palletTwo}");
 }
