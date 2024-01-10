3cf0fe2067d2e6f2261d5b5fd584bff18b310f72
Andrea <andrea@interhact.net>
2024-01-10 12:41:40 -0600

    sorting simple array

---
+++ arrays_and_helpers/Program.cs
@@ -0,0 +1,10 @@
+﻿//create array of pallets, then sort them
+
+string[] pallets = { "B14", "A11", "B12", "A13" };
+
+Console.WriteLine("Sorted...");
+Array.Sort(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
