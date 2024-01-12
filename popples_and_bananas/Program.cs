20004357e6f4334946d1573d83f179501d3beb57
Andrea <andrea@interhact.net>
2024-01-12 15:19:32 -0600

    work on demonstration

--- popples_and_bananas/Program.cs
+++ popples_and_bananas/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Reflection.Metadata;
+/*
+We're going to start off simple.
+Here we have a banana, a potato, and an apple.
+The variable names are like signs on a countertop. 
+The values are the physical objects.
+*/
+
+/*
+Image 1:
+banana with a card that says "banana"
+potato with a card that says "potato"
+apple with a card that says "otherFruit"
+*/
+
+/*
+This is 
+*/
+string banana = "banana";
+string potato = "potato";
+string otherFruit = "orange";
+
+Console.WriteLine("Global Scope(outside before ShowMeBlue):");
+Console.WriteLine($"The banana is: {banana}.");
+Console.WriteLine($"The potato is: {potato}.");
+Console.WriteLine($"The other fruit is: {otherFruit}.");
+Console.WriteLine("");
+
+string ShowMeBlue(string item){
+    potato = "apple";
+    return "blue " + item;
+}
+
+string ShowMeStripes(string item){
+    return "striped " + item;
+}
+
+Console.WriteLine("-------");
+Console.WriteLine("");
+Console.WriteLine("Local Scope(inside during ShowMeBlue):");
+Console.WriteLine(ShowMeBlue(banana));
+Console.WriteLine(ShowMeBlue(potato));
+Console.WriteLine(ShowMeBlue(otherFruit));
+Console.WriteLine("");
+
+Console.WriteLine("-------");
+Console.WriteLine("");
+Console.WriteLine("Local Scope(inside during ShowMeStripes):");
+Console.WriteLine(ShowMeStripes(ShowMeBlue(banana)));
+Console.WriteLine(ShowMeBlue(ShowMeStripes(potato)));
+
+
+
+Console.WriteLine("------");
+Console.WriteLine("");
+Console.WriteLine("Global Scope(outside after ShowMeBlue and ShowMeStripes)");
+Console.WriteLine($"The banana is: {banana}.");
+Console.WriteLine($"The potato is: {potato}.");
+Console.WriteLine($"The other fruit is: {otherFruit}.");
