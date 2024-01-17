af69385386b77c6cf9e978d03477c6b99fdbe18b
Andrea <andrea@interhact.net>
2024-01-17 14:05:28 -0600

    work on string interpolation

--- string_formatting_basics/Program.cs
+++ string_formatting_basics/Program.cs
@@ -1,4 +1,18 @@
-﻿string first = "Hello";
-string seconde = "World";
-string result = string.Format("{0} {1}!", first, seconde);
+﻿//composite formatting
+
+/*
+string first = "Hello";
+string second = "World";
+string result = string.Format("{0} {1}!", first, second);
 Console.WriteLine(result);
+Console.WriteLine("{0} {0} {0}", first, second);
+*/
+
+//string interpolation
+
+string first = "Hello";
+string second = "World";
+Console.WriteLine($"{first} {second}!");
+Console.WriteLine($"{second} {first}!");
+Console.WriteLine($"{first} {first} {first}!");
+
