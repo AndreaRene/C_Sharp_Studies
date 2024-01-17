c9334a8c2d12a82686464464cd19b788d95bc55a
Andrea <andrea@interhact.net>
2024-01-17 14:08:21 -0600

    formatting currency

--- string_formatting_basics/Program.cs
+++ string_formatting_basics/Program.cs
@@ -1,18 +1,20 @@
 ﻿//composite formatting
 
-/*
 string first = "Hello";
 string second = "World";
 string result = string.Format("{0} {1}!", first, second);
 Console.WriteLine(result);
 Console.WriteLine("{0} {0} {0}", first, second);
-*/
 
 //string interpolation
 
-string first = "Hello";
-string second = "World";
 Console.WriteLine($"{first} {second}!");
 Console.WriteLine($"{second} {first}!");
 Console.WriteLine($"{first} {first} {first}!");
+
+//formatting currency
+
+decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save {discount:C})");
 
