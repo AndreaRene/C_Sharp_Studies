276385ad200b317f9090bbf9644cdec6f733c85e
Andrea <andrea@interhact.net>
2024-01-17 13:32:25 -0600

    work on reversing words

--- reverse_words_challenge/Program.cs
+++ reverse_words_challenge/Program.cs
@@ -1,11 +1,19 @@
-﻿string pangram = "The quick brown fox jumps over the lazy dog";
+﻿
+string pangram = "The quick brown fox jumps over the lazy dog";
 
 //printed result: ehT kciuq nworb xof spmuj revo eht yzal god
 
 //split to wordArray
 //["The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"]
+string[] wordsArray = pangram.Split(' ');
 
+List<string> reversedWords = new List<string>();
+Console.WriteLine( wordsArray);
 //loop through wordArray
+foreach (string word in wordsArray){
+    char[] letterArray = word.ToCharArray();
+    // Console.WriteLine();
+
     //ToCharArray to letterArray
         //["T", "h", "e"]
     //reverse letterArray
@@ -16,7 +24,7 @@
             //["ehT", "kciuq", "nworb", "xof", "spmuj", "revo", "eht", "yzal", "god"]
     //join to new string?
         //"ehT kciuq nworb xof spmuj revo eht yzal god"
-
+}
 
 
 //Sort (array ascending), Reverse (array decending), Clear(elements in array), Resize(elements in array(bigger or smaller)), ToCharArray(string to new array of indiv chars) Split (array to indiv arrays), and Join (array elements into string)
