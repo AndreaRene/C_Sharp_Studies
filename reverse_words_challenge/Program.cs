f11cb2bff700229ff10e8b959d1fd2c91ca72f8b
Andrea <andrea@interhact.net>
2024-01-17 11:24:31 -0600

    just a bunch of updated stuff?

---
+++ reverse_words_challenge/Program.cs
@@ -0,0 +1,22 @@
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+
+//printed result: ehT kciuq nworb xof spmuj revo eht yzal god
+
+//split to wordArray
+//["The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"]
+
+//loop through wordArray
+    //ToCharArray to letterArray
+        //["T", "h", "e"]
+    //reverse letterArray
+        //["e", "h", "T"]
+    //join to reverseWordString
+        // "ehT"
+    //append to new array?
+            //["ehT", "kciuq", "nworb", "xof", "spmuj", "revo", "eht", "yzal", "god"]
+    //join to new string?
+        //"ehT kciuq nworb xof spmuj revo eht yzal god"
+
+
+
+//Sort (array ascending), Reverse (array decending), Clear(elements in array), Resize(elements in array(bigger or smaller)), ToCharArray(string to new array of indiv chars) Split (array to indiv arrays), and Join (array elements into string)
