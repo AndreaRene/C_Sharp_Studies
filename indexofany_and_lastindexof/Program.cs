08b5e3b0c9fe79fc754179eb72d289722d577056
Andrea <andrea@interhact.net>
2024-01-19 09:24:23 -0600

    find the last occourance of a character

---
+++ indexofany_and_lastindexof/Program.cs
@@ -0,0 +1,10 @@
+﻿//find the last occourance of a character
+
+string message = "(What if) I am (only interested) in the last (set of parentheses)?";
+int openingPosition = message.LastIndexOf('(');
+
+openingPosition += 1;
+int closingPosition = message.LastIndexOf(')');
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
+
