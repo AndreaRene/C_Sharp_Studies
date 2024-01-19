26e69a8f6cef06cf181a67a6e427c7cd72b667b4
Andrea <andrea@interhact.net>
2024-01-19 09:40:17 -0600

    Remove characters in specific locations from a string

---
+++ remove_and_replace/Program.cs
@@ -0,0 +1,6 @@
+﻿//remove characters in specific locations from a string
+
+string data = "12345John Smith          5000  3  ";
+string updatedData = data.Remove(5, 20);
+Console.WriteLine(updatedData);
+
