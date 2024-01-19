746572e592789652c833daa266ead181a2e24ea4
Andrea <andrea@interhact.net>
2024-01-19 10:58:15 -0600

    part one of challenge extract quantity complete

---
+++ extract_replace_remove_challenge/Program.cs
@@ -0,0 +1,18 @@
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+
+int openingSpanPosition = input.IndexOf(openSpan);
+int closingSpanPosition = input.IndexOf(closeSpan);
+
+openingSpanPosition += openSpan.Length;
+int length = closingSpanPosition - openingSpanPosition;
+
+quantity = input.Substring(openingSpanPosition, length);
+Console.WriteLine(quantity);
+Console.WriteLine(output);
