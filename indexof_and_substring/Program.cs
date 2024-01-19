2c1bc55f70ac06d1add45c0be1bba5e2c774570a
Andrea <andrea@interhact.net>
2024-01-19 09:18:50 -0600

    utilize const to maintain integrity and uniformity

--- indexof_and_substring/Program.cs
+++ indexof_and_substring/Program.cs
@@ -2,15 +2,40 @@
 
 string message = "Find what is (inside the parentheses)";
 
-int openingPosition = message.IndexOf('(');
-int closingPosition = message.IndexOf(')');
+// int openingPosition = message.IndexOf('(');
+// int closingPosition = message.IndexOf(')');
 
-Console.WriteLine(openingPosition);
-Console.WriteLine(closingPosition);
+// Console.WriteLine(openingPosition);
+// Console.WriteLine(closingPosition);
 
 //use substring to extract the message
 
 openingPosition += 1;
 
+// int length = closingPosition - openingPosition;
+// Console.WriteLine(message.Substring(openingPosition, length));
+
+//increasing opening position
+
+// string message = "What is the value <span>between the tags</span>?";
+
+// int openingPosition = message.IndexOf("<span>");
+// int closingPosition = message.IndexOf("</span>");
+
+// openingPosition += 6;
+// int length = closingPosition - openingPosition;
+// Console.WriteLine(message.Substring(openingPosition, length));
+
+//misspelling could impact performance
+
+string message = "What is the value <span>between the tags</span>?";
+
+const string openSpan = "<span>";//set a const
+const string closeSpan = "</span>";//set a const
+
+int openingPosition = message.IndexOf(openSpan);
+int closingPosition = message.IndexOf(closeSpan);
+
+openingPosition += openSpan.Length;
 int length = closingPosition - openingPosition;
 Console.WriteLine(message.Substring(openingPosition, length));
