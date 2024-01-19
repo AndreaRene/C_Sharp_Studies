fb97195fd5a99d89f9308a2eef92a5481cb70cf1
Andrea <andrea@interhact.net>
2024-01-19 09:21:22 -0600

    indexof and substring lesson complete

--- indexof_and_substring/Program.cs
+++ indexof_and_substring/Program.cs
@@ -39,3 +39,11 @@ int closingPosition = message.IndexOf(closeSpan);
 openingPosition += openSpan.Length;
 int length = closingPosition - openingPosition;
 Console.WriteLine(message.Substring(openingPosition, length));
+
+/*
+IndexOf() gives you the first position of a character or string inside of another string.
+IndexOf() returns -1 if it can't find a match.
+Substring() returns just the specified portion of a string, using a starting position and optional length.
+There's often more than one way to solve a problem. You used two separate techniques to find all instances of a given character or string.
+Avoid hardcoded magic values. Instead, define a const variable. A constant variable's value can't be changed after initialization.
+*/
