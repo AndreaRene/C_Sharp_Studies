69cae69f3d4787ee03c0ae5c2fbe1d10fe4900d7
Andrea <andrea@interhact.net>
2024-01-19 09:28:43 -0600

    using a loop to find all instances of a character/substring

--- indexofany_and_lastindexof/Program.cs
+++ indexofany_and_lastindexof/Program.cs
@@ -1,10 +1,27 @@
-﻿//find the last occourance of a character
+﻿//find the last occourance of a character/substring
 
-string message = "(What if) I am (only interested) in the last (set of parentheses)?";
-int openingPosition = message.LastIndexOf('(');
+// string message = "(What if) I am (only interested) in the last (set of parentheses)?";
+// int openingPosition = message.LastIndexOf('(');
 
-openingPosition += 1;
-int closingPosition = message.LastIndexOf(')');
-int length = closingPosition - openingPosition;
-Console.WriteLine(message.Substring(openingPosition, length));
+// openingPosition += 1;
+// int closingPosition = message.LastIndexOf(')');
+// int length = closingPosition - openingPosition;
+// Console.WriteLine(message.Substring(openingPosition, length));
 
+//retrieve all occourances of a character/substring
+
+string message = "(What if) there are (more than) one (set of parentheses)?";
+while (true)
+{
+    int openingPosition = message.IndexOf('(');
+    if (openingPosition == -1) break;
+
+    openingPosition += 1;
+    int closingPosition = message.IndexOf(')');
+    int length = closingPosition - openingPosition;
+    Console.WriteLine(message.Substring(openingPosition, length));
+
+    // Note the overload of the Substring to return only the remaining 
+    // unprocessed message:
+    message = message.Substring(closingPosition + 1); //reassigning the value of message to exclude the previous substring
+}
