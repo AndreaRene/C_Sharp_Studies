8c61ddcaffad012576bdb03822d9b6d5bc8e5134
Andrea <andrea@interhact.net>
2024-01-19 09:07:42 -0600

    using index of to find particular start and endpoint

---
+++ indexof_and_substring/Program.cs
@@ -0,0 +1,10 @@
+﻿//find parenthesis pairs inside a string
+
+string message = "Find what is (inside the parentheses)";
+
+int openingPosition = message.IndexOf('(');
+int closingPosition = message.IndexOf(')');
+
+Console.WriteLine(openingPosition);
+Console.WriteLine(closingPosition);
+
