2ead3585f609204e5d3ef6b4d040351c67fcf4e3
Andrea <andrea@interhact.net>
2024-01-23 11:51:10 -0600

    return values lesson

--- methods_return_values/Program.cs
+++ methods_return_values/Program.cs
@@ -1,22 +1,31 @@
-﻿double total = 0;
+﻿//return values
+
+double total = 0;
 double minimumSpend = 30.00;
 
 double[] items = {15.97, 3.50, 12.25, 22.99, 10.98};
 double[] discounts = {0.30, 0.00, 0.10, 0.20, 0.50};
 
-Console.WriteLine($"Total: ${total}");
-
-void GetDiscountedPrice(int itemIndex)
+for (int i = 0; i < items.Length; i++)
 {
-    // Calculate the discounted price of the item
+    total += GetDiscountedPrice(i);
 }
 
-void TotalMeetsMinimum()
+total -= TotalMeetsMinimum() ? 5.00 : 0.00;
+
+Console.WriteLine($"Total: ${FormatDecimal(total)}");
+
+double GetDiscountedPrice(int itemIndex)
 {
-    // Check if the total meets the minimum
+    return items[itemIndex] * (1 - discounts[itemIndex]);
 }
 
-void FormatDecimal(double input)
+bool TotalMeetsMinimum()
 {
-    // Format the double so only 2 decimal places are displayed
+    return total >= minimumSpend;
+}
+
+string FormatDecimal(double input)
+{
+    return input.ToString().Substring(0, 5);
 }
