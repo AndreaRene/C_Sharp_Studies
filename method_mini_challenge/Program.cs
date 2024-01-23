57961a817f96ef840df295ea3bee2e25847872b5
Andrea <andrea@interhact.net>
2024-01-23 12:28:23 -0600

    mini challenge complete

--- method_mini_challenge/Program.cs
+++ method_mini_challenge/Program.cs
@@ -6,20 +6,46 @@ if (ShouldPlay())
     PlayGame();
 }
 
+bool ShouldPlay()
+{
+    string response = Console.ReadLine();
+    return response.ToLower().Equals("y");
+}
+
+
+
 void PlayGame() 
 {
     var play = true;
 
     while (play) 
     {
-        var target;
-        var roll;
+        var target = TargetRoll();
+        var roll = PlayerRoll();
 
         Console.WriteLine($"Roll a number greater than {target} to win!");
         Console.WriteLine($"You rolled a {roll}");
-        Console.WriteLine(WinOrLose());
+        Console.WriteLine(WinOrLose(roll, target));
         Console.WriteLine("\nPlay again? (Y/N)");
 
         play = ShouldPlay();
     }
+}
+
+int TargetRoll()
+{
+    return random.Next(1, 6);
+}
+int PlayerRoll()
+{
+    return random.Next(1, 7);
+}
+
+string WinOrLose(int roll, int target)
+{
+    if (roll > target)
+    {
+        return "You Win!";
+    }
+    return "You lose!";
 }
