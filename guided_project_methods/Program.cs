329f74cd1e8478ddc82cdec3f922acf6f8930f08
Andrea <andrea@interhact.net>
2024-01-23 12:38:27 -0600

    setup randomize animals method

--- guided_project_methods/Program.cs
+++ guided_project_methods/Program.cs
@@ -6,3 +6,30 @@ string[] pettingZoo =
     "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
+
+// RandomizeAnimals();
+
+
+// string[,] group = AssignGroup();
+Console.WriteLine("School A");
+// PrintGroup(group);
+
+void RandomizeAnimals()
+{
+    Random random = new Random();
+
+    for (int i = 0; i < pettingZoo.Length; i++)
+    {
+        //Fisher-Yates shuffle algorithm
+        int r = random.Next(i, pettingZoo.Length);
+
+        string temp = pettingZoo[r];
+        pettingZoo[r] = pettingZoo[i];
+        pettingZoo[i] = temp;
+    }
+}
+
+foreach(string animal in pettingZoo) 
+{
+    Console.WriteLine(animal);
+}
