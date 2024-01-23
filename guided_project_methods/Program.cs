8c29b6d42717be38cff7d9b2e6bbb2f61b3d65d7
Andrea <andrea@interhact.net>
2024-01-23 12:41:22 -0600

    assign group method

--- guided_project_methods/Program.cs
+++ guided_project_methods/Program.cs
@@ -7,20 +7,17 @@ string[] pettingZoo =
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
 
-// RandomizeAnimals();
-
-
-// string[,] group = AssignGroup();
+RandomizeAnimals();
+string[,] group = AssignGroup();
 Console.WriteLine("School A");
 // PrintGroup(group);
 
-void RandomizeAnimals()
+void RandomizeAnimals() 
 {
     Random random = new Random();
 
-    for (int i = 0; i < pettingZoo.Length; i++)
+    for (int i = 0; i < pettingZoo.Length; i++) 
     {
-        //Fisher-Yates shuffle algorithm
         int r = random.Next(i, pettingZoo.Length);
 
         string temp = pettingZoo[r];
@@ -29,7 +26,18 @@ void RandomizeAnimals()
     }
 }
 
-foreach(string animal in pettingZoo) 
+string[,] AssignGroup(int groups = 6) 
 {
-    Console.WriteLine(animal);
+    string[,] result = new string[groups, pettingZoo.Length/groups];
+    int start = 0;
+
+    for (int i = 0; i < groups; i++) 
+    {
+        for (int j = 0; j < result.GetLength(1); j++) 
+        {
+            result[i,j] = pettingZoo[start++];
+        }
+    }
+
+    return result;
 }
--- guided_project_methods/obj/Debug/net8.0/guided_project_methods.AssemblyInfo.cs
+++ guided_project_methods/obj/Debug/net8.0/guided_project_methods.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("guided_project_methods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+329f74cd1e8478ddc82cdec3f922acf6f8930f08")]
 [assembly: System.Reflection.AssemblyProductAttribute("guided_project_methods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("guided_project_methods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
